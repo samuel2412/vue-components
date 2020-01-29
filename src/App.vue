ed327aa096df11f0e8967b2890ada55206d270e8
Samuel Alves <samuel241296@hotmail.com>
2020-01-29 13:41:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+      <app-servers ></app-servers>
+  </div>
+</template>
+
+<script>
+</script>
---
+++ src/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <app-server-status v-for="server in 5" />
+  </div>
+</template>
+
+<script>
+import ServerStatus from "./ServerStatus.vue";
+export default {
+  components: {
+    "app-server-status": ServerStatus
+  }
+};
+</script>
+
+<style>
+</style>
